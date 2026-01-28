e4f671ee810d85ead4d4d342ae0441ffd49ba3ec
Ahmed Ahmed <ahmed.lola2018@gmail.com>
2026-01-28 19:11:51 +0200

    Add Animation To Splash View.

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,9 +1,36 @@
 import 'package:first/core/utils/assets.dart';
 import 'package:flutter/cupertino.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
 
+class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    super.initState();
+    animationController=AnimationController(
+        vsync: this,
+        duration: Duration(seconds: 1)
+    );
+    slidingAnimation= Tween<Offset>(begin: const Offset(0, 10) ,end:Offset.zero ).animate(animationController);
+    animationController.forward();
+    slidingAnimation.addListener((){
+      setState(() {
+
+      });
+    });
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    animationController.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -12,8 +39,16 @@ class SplashViewBody extends StatelessWidget {
       children: [
         Image.asset(AssetsData.logo),
         const SizedBox(height: 4,),
-        const Text('Read Free Box',
-        textAlign: TextAlign.center,)
+        AnimatedBuilder(
+          animation:slidingAnimation,
+          builder: (context,_){
+            return  SlideTransition(
+            position: slidingAnimation,
+            child: const Text('Read Free Box',
+              textAlign: TextAlign.center,),
+    );
+    },
+        ),
       ],
     );
   }
