a146f0db447b40ae70ae5a555c11578cc77359b1
Ahmed Ahmed <ahmed.lola2018@gmail.com>
2026-02-13 20:32:46 +0200

    create book entity.

---
+++ lib/features/home/doman/entities/book_entity.dart
@@ -0,0 +1,9 @@
+class BookEntity {
+  final String image;
+  final String title;
+  final String authorName;
+  final num ratting;
+  final num price;
+
+  BookEntity(this.image, this.title, this.authorName, this.ratting, this.price);
+}
