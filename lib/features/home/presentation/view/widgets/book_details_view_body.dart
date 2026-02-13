8f4e5e4a2b3abd55d39b38794a98874004d06a96
Ahmed Ahmed <ahmed.lola2018@gmail.com>
2026-02-13 17:50:02 +0200

    add similar books list view to book details.

--- lib/features/home/presentation/view/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/view/widgets/book_details_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:first/core/utils/styles.dart';
 import 'package:first/features/home/presentation/view/widgets/book_rating.dart';
 import 'package:first/features/home/presentation/view/widgets/custom_book_image.dart';
+import 'package:first/features/home/presentation/view/widgets/similar_books_list_view.dart';
 import 'package:flutter/material.dart';
 
 import 'books_action.dart';
@@ -21,13 +22,13 @@ class BookDetailsViewBody extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: width * .17),
             child: CustomBookImage(),
           ),
-          SizedBox(height: 42),
+          const SizedBox(height: 42),
           Text(
             'Star Wars Return of the Jedi',
             style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
             overflow: TextOverflow.ellipsis,
           ),
-          SizedBox(height: 13),
+          const SizedBox(height: 13),
           Opacity(
             opacity: .7,
             child: Text(
@@ -38,10 +39,17 @@ class BookDetailsViewBody extends StatelessWidget {
               ),
             ),
           ),
-          SizedBox(height: 20),
-          BookRating(mainAxisAlignment: MainAxisAlignment.center),
-          SizedBox(height: 41),
-          BookAction(),
+          const SizedBox(height: 20),
+          const BookRating(mainAxisAlignment: MainAxisAlignment.center),
+          const SizedBox(height: 41),
+          const BookAction(),
+          const SizedBox(height: 50),
+          const Align(
+            alignment: Alignment.centerLeft,
+            child: Text('You can also like', style: Styles.textStyle16),
+          ),
+          const SizedBox(height: 20),
+          SimilarBookListView(),
         ],
       ),
     );
--- lib/features/home/presentation/view/widgets/custom_book_image.dart
+++ lib/features/home/presentation/view/widgets/custom_book_image.dart
@@ -8,12 +8,12 @@ class CustomBookImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AspectRatio(
-      aspectRatio: 2.8/4,
+      aspectRatio: 2.8 / 4,
       child: Container(
-        width:50 ,
+        width: 50,
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            image: DecorationImage(image: AssetImage(AssetsData.testImag))
+          borderRadius: BorderRadius.circular(20),
+          image: DecorationImage(image: AssetImage(AssetsData.testImag)),
         ),
       ),
     );
