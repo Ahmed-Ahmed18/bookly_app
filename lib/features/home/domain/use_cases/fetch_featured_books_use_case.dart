115158b0c3850e16892443c9bc7ca94b45af6010
Ahmed Ahmed <ahmed.lola2018@gmail.com>
2026-09-25 14:24:12 +0300

    add generic use case.

--- lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -1,18 +1,19 @@
+
 import 'package:booky_app/features/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
 import '../../../../core/errors/failure.dart';
+import '../../../../core/use_cases/no_param_use_case.dart';
 import '../entities/book_entity.dart';
 
-class FetchFeaturedBooksUseCase {
+class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>> {
   final HomeRepo homeRepo;
 
   FetchFeaturedBooksUseCase(this.homeRepo);
 
-  Future<Either<Failure,List<BookEntity>>>fetchFeaturedBooks(){
-
-    // check permission
-    return homeRepo.fetchNewestBooks();
+  @override
+  Future<Either<Failure, List<BookEntity>>> call() {
+    // request permission
+  return homeRepo.fetchNewestBooks();
   }
-
 }
