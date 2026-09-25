1ecbe5f623f76fbdc70c25753d447e2db3828e52
Ahmed Ahmed <ahmed.lola2018@gmail.com>
2026-09-25 10:19:07 +0300

    add fetch featured books use case.

--- lib/features/home/domain/repos/home_repo.dart
+++ lib/features/home/domain/repos/home_repo.dart
@@ -1,14 +1,9 @@
 import 'dart:async';
-
-import 'package:booky_app/core/errors/failure.dart';
-import 'package:booky_app/core/errors/failure.dart';
 import 'package:booky_app/core/errors/failure.dart';
 import 'package:booky_app/features/home/domain/entities/book_entity.dart';
-import 'package:dartz/dartz.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
  Future<Either<Failure,List<BookEntity>>>fetchFeaturedBooks();
  Future<Either<Failure,List<BookEntity>>>fetchNewestBooks();
-
 }
---
+++ lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -0,0 +1,18 @@
+import 'package:booky_app/features/home/domain/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failure.dart';
+import '../entities/book_entity.dart';
+
+class FetchFeaturedBooksUseCase {
+  final HomeRepo homeRepo;
+
+  FetchFeaturedBooksUseCase(this.homeRepo);
+
+  Future<Either<Failure,List<BookEntity>>>fetchFeaturedBooks(){
+
+    // check permission
+    return homeRepo.fetchNewestBooks();
+  }
+
+}
