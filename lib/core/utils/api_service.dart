9f80adc3c2fd1377b02177f6992bbdf475fda557
Ahmed Ahmed <ahmed.lola2018@gmail.com>
2026-09-26 09:52:09 +0300

    implement remote fetch featured books.

--- lib/core/utils/api_service.dart
+++ lib/core/utils/api_service.dart
@@ -1,12 +1,16 @@
 import 'package:dio/dio.dart';
 
 class ApiService {
+  static const apiKey='AIzaSyD77s1gxce0QuZ-ngVNutjVvcHB7VHOzc0';
   final Dio _dio;
   final baseUrl='https://www.googleapis.com/books/v1/';
   ApiService(this._dio);
 
  Future<Map<String,dynamic>> get({required String endpoint})async{
-  var response= await _dio.get('$baseUrl$endpoint');
+  var response= await _dio.get('$baseUrl$endpoint',
+    queryParameters: {
+      'key': apiKey,
+    },);
   return response.data;
   }
-} 
+}
--- lib/features/home/data/data_sources/home_remote_data_source.dart
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -1,3 +1,7 @@
+import 'package:booky_app/core/utils/api_service.dart';
+import 'package:booky_app/features/home/data/models/book_model/BookModel.dart';
+import 'package:dartz/dartz_unsafe.dart';
+
 import '../../domain/entities/book_entity.dart';
 
 abstract class HomeRemoteDataSource {
@@ -5,10 +9,23 @@ abstract class HomeRemoteDataSource {
   Future<List<BookEntity>>fetchNewestBooks();
 }
 class HomeRemoteDataSourceImpl extends HomeRemoteDataSource{
+  final ApiService apiService;
+
+  HomeRemoteDataSourceImpl(this.apiService);
+
   @override
-  Future<List<BookEntity>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<List<BookEntity>> fetchFeaturedBooks() async{
+ var data= await apiService.get(endpoint: 'volumes?filter=free-ebooks&q=programming');
+ List<BookEntity> books = getBookList(data);
+ return books;
+  }
+
+  List<BookEntity> getBookList(Map<String, dynamic> data) {
+    List<BookEntity>books=[];
+    for(var bookMap in data['items']){
+      books.add(BookModel.fromJson(bookMap));
+    }
+    return books;
   }
 
   @override
