990948214ee5f45a9514f08ddf6eedfb70c910cd
Ahmed Ahmed <ahmed.lola2018@gmail.com>
2026-09-26 15:21:20 +0300

    create hive featured box register.

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,3 +3,4 @@ import 'dart:ui';
 const kPrimaryColor= Color(0xff100B20);
 const kNavigationDuration =Duration(milliseconds: 250);
 const kGtSectrafine='GtSectraFine';
+const KFeaturedBox='feature_box';
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:booky_app/constants.dart';
 import 'package:booky_app/core/utils/app-router.dart';
+import 'package:booky_app/features/home/domain/entities/book_entity.dart';
 import 'package:booky_app/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive/hive.dart';
+import 'constants.dart';
 
-void main() {
+void main() async{
   runApp(const Bookly());
+  Hive.registerAdapter(BookEntityAdapter());
+  await Hive.openBox(KFeaturedBox);
 }
 
 class Bookly extends StatelessWidget {
