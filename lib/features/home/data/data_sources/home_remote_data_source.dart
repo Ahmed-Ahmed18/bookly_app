a16ca78723fd05f0376a81c91587e17539f2fbcf
Ahmed Ahmed <ahmed.lola2018@gmail.com>
2026-09-26 15:34:08 +0300

    save_featured_books_data.

--- lib/features/home/data/data_sources/home_remote_data_source.dart
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -1,6 +1,9 @@
+import 'package:booky_app/constants.dart';
 import 'package:booky_app/core/utils/api_service.dart';
 import 'package:booky_app/features/home/data/models/book_model/BookModel.dart';
+import 'package:hive/hive.dart';
 
+import '../../../../core/utils/functions/save_books_data.dart';
 import '../../domain/entities/book_entity.dart';
 
 abstract class HomeRemoteDataSource {
@@ -16,8 +19,10 @@ class HomeRemoteDataSourceImpl extends HomeRemoteDataSource{
   Future<List<BookEntity>> fetchFeaturedBooks() async{
  var data= await apiService.get(endpoint:'volumes?filter=free-ebooks&q=programming');
  List<BookEntity> books = getBookList(data);
+ saveBooksData(books,KFeaturedBox);
  return books;
   }
+
 
   @override
   Future<List<BookEntity>> fetchNewestBooks()async {
